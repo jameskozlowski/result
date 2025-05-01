396f4c98dcb0516d1736e801e68247d73155f47d
James Kozlowski <jameskozlowski@gmail.com>
2025-05-01 19:47:13 -0300

    add documentation

--- result.go
+++ result.go
@@ -11,24 +11,33 @@ type Result[T any] struct {
 }
 
 // Constructors
+
+// Ok creates a new Result with a successful value and no error.
 func Ok[T any](val T) Result[T] {
 	return Result[T]{Value: val, Err: nil}
 }
 
+// Err creates a new Result with an error and no value.
 func Err[T any](msg string) Result[T] {
 	return Result[T]{Err: errors.New(msg)}
 }
 
 // Status checks
+
+// IsOk returns true if the Result contains a value and no error.
 func (r Result[T]) IsOk() bool {
 	return r.Err == nil
 }
 
+// IsErr returns true if the Result contains an error.
 func (r Result[T]) IsErr() bool {
 	return r.Err != nil
 }
 
 // Unwrapping
+
+// Unwrap returns the value contained in the Result.
+// Panics if the Result contains an error.
 func (r Result[T]) Unwrap() T {
 	if r.IsErr() {
 		panic(r.Err)
@@ -36,6 +45,7 @@ func (r Result[T]) Unwrap() T {
 	return r.Value
 }
 
+// UnwrapOr returns the value contained in the Result, or the provided default value if the Result contains an error.
 func (r Result[T]) UnwrapOr(defaultValue T) T {
 	if r.IsErr() {
 		return defaultValue
@@ -43,6 +53,7 @@ func (r Result[T]) UnwrapOr(defaultValue T) T {
 	return r.Value
 }
 
+// UnwrapOrElse returns the value contained in the Result, or the result of the provided function if the Result contains an error.
 func (r Result[T]) UnwrapOrElse(f func() T) T {
 	if r.IsErr() {
 		return f()
@@ -50,6 +61,7 @@ func (r Result[T]) UnwrapOrElse(f func() T) T {
 	return r.Value
 }
 
+// UnwrapOrDefault returns the value contained in the Result, or the zero value of the type if the Result contains an error.
 func (r Result[T]) UnwrapOrDefault() T {
 	var zero T
 	if r.IsErr() {
@@ -59,6 +71,9 @@ func (r Result[T]) UnwrapOrDefault() T {
 }
 
 // Mapping
+
+// Map applies the provided function to the value contained in the Result, if it is successful.
+// Returns a new Result with the transformed value or the original error.
 func (r Result[T]) Map(f func(T) T) Result[T] {
 	if r.IsErr() {
 		return r
@@ -66,6 +81,8 @@ func (r Result[T]) Map(f func(T) T) Result[T] {
 	return Ok(f(r.Value))
 }
 
+// MapErr applies the provided function to the error contained in the Result, if it contains an error.
+// Returns a new Result with the transformed error or the original value.
 func (r Result[T]) MapErr(f func(error) error) Result[T] {
 	if r.IsOk() {
 		return r
@@ -73,6 +90,8 @@ func (r Result[T]) MapErr(f func(error) error) Result[T] {
 	return Result[T]{Err: f(r.Err)}
 }
 
+// AndThen applies the provided function to the value contained in the Result, if it is successful.
+// Returns the Result returned by the function or the original error.
 func (r Result[T]) AndThen(f func(T) Result[T]) Result[T] {
 	if r.IsErr() {
 		return r
@@ -80,6 +99,8 @@ func (r Result[T]) AndThen(f func(T) Result[T]) Result[T] {
 	return f(r.Value)
 }
 
+// OrElse applies the provided function to the error contained in the Result, if it contains an error.
+// Returns the Result returned by the function or the original value.
 func (r Result[T]) OrElse(f func(error) Result[T]) Result[T] {
 	if r.IsOk() {
 		return r
@@ -88,6 +109,9 @@ func (r Result[T]) OrElse(f func(error) Result[T]) Result[T] {
 }
 
 // Expect-style panics
+
+// Expect returns the value contained in the Result.
+// Panics with the provided message if the Result contains an error.
 func (r Result[T]) Expect(msg string) T {
 	if r.IsErr() {
 		panic(fmt.Sprintf("%s: %v", msg, r.Err))
@@ -95,6 +119,8 @@ func (r Result[T]) Expect(msg string) T {
 	return r.Value
 }
 
+// ExpectErr returns the error contained in the Result.
+// Panics with the provided message if the Result contains a value.
 func (r Result[T]) ExpectErr(msg string) error {
 	if r.IsOk() {
 		panic(msg)
@@ -102,6 +128,8 @@ func (r Result[T]) ExpectErr(msg string) error {
 	return r.Err
 }
 
+// UnwrapErr returns the error contained in the Result.
+// Panics if the Result contains a value.
 func (r Result[T]) UnwrapErr() error {
 	if r.IsOk() {
 		panic("no error to unwrap")
